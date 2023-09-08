8d74651d111a58a3a2efa9721df680ae0c670d7d
Swaraj Barnwal <ikku12piku@gmail.com>
2023-09-08 08:11:25 +0545

    Hyperlinked the room.html and home.html

--- base/views.py
+++ base/views.py
@@ -8,10 +8,15 @@ rooms= [
 ]
 
 def home(request):
-    return render(request,'home.html', {'rooms': rooms})
+    context = {'rooms': rooms}
+    return render(request, 'home.html', context)
 
-def room(request):
-    return render(request, 'room.html')
+def room(request,pk):
+    room = None
+    for i in rooms:
+        if i['id'] == pk:
+            room = i
+    context = {'room' : room}
+    return render(request, 'room.html', context)
 
-def test():
-    return 1
+
